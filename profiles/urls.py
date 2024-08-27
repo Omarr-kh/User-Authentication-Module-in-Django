8b8c637a5ceec750ad9b0431848df8b8c99a109c
Omar <omarkhaled1752003@yahoo.com>
2024-08-27 18:53:38 +0300

    urls for password resetting

--- profiles/urls.py
+++ profiles/urls.py
@@ -8,21 +8,29 @@ urlpatterns = [
     path("login/", views.login_page, name="login"),
     path("logout/", views.logout_page, name="logout"),
     path(
-        "reset_password/", auth_views.PasswordResetView.as_view(), name="reset_password"
+        "reset_password/",
+        auth_views.PasswordResetView.as_view(template_name="password_reset.html"),
+        name="reset_password",
     ),
     path(
         "reset_password_sent/",
-        auth_views.PasswordResetDoneView.as_view(),
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="password_reset_sent.html"
+        ),
         name="password_reset_done",
     ),
     path(
         "reset/<uidb64>/<token>/",
-        auth_views.PasswordResetConfirmView.as_view(),
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="password_reset_form.html"
+        ),
         name="password_reset_confirm",
     ),
     path(
         "reset_password_complete/",
-        auth_views.PasswordResetCompleteView.as_view(),
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="reset_password_complete.html"
+        ),
         name="password_reset_complete",
     ),
 ]
