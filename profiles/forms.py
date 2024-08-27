0af0e58cd7065ed4aa7f61a8b08e8a2c32ccdb96
Omar <omarkhaled1752003@yahoo.com>
2024-08-27 15:06:14 +0300

    register, and login forms

---
+++ profiles/forms.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+
+
+class RegisterForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=200, label="username")
+    password = forms.CharField(widget=forms.PasswordInput, label="Password")
